be1ac6537dd274e71627471a0403b53f655edb31
nityaravi <32396647+nityaravi@users.noreply.github.com>
2020-01-13 21:56:48 -0500

    Adding index.js

---
+++ node-example-npm/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+
+const port = 3000;
+const server = app.listen(port, () => {
+        console.log('listening on port %s.\n', server.address().port);
+    });
+
+module.exports = app;
