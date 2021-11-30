7b225c4ea0016402dd1ad5539a357daec6732c31
Katie McLaughlin <katie@glasnt.com>
2021-11-30 14:35:38 +1100

    init sql-private-pool

---
+++ sql-private-pool/migrate.py
@@ -0,0 +1,31 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import app
+
+
+def migrate():
+    try:
+        db = app.connect_db()
+        with db.connect() as conn:
+            row = conn.execute("SELECT NOW() as now").fetchone()
+            data = dict(row)
+            return data["now"]
+    except Exception as e:
+        return f"Connection not successful: {e}"
+
+
+print("Apply migration...")
+print("Migration result:", migrate())
+print("Success.")
