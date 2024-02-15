b124292e998fa08919c6485c94eb6766c0cdc1e6
ramonmengarda <ramon_hiago@hotmail.com>
2024-02-15 15:05:25 -0300

    cc

--- python-example-flask/main.py
+++ python-example-flask/main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return f"Hello {name}! RFM-36"
+    return f"Hello {name}! RFM-38"
 
 
 if __name__ == "__main__":
